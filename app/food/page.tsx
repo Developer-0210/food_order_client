e427fa86fd09c3d817ca131743df15358e907264
Hari-singh-Joshi <132601298+Hari-singh-Joshi@users.noreply.github.com>
2025-07-15 03:52:47 +0530

    agian

--- app/food/page.tsx
+++ app/food/page.tsx
@@ -1,8 +1,5 @@
-import dynamic from "next/dynamic"
 import { Suspense } from "react"
-
-const FoodClient = dynamic(() => import("./FoodClient"), { ssr: false })
-
+import FoodClient from "./FoodClient"
 export default function FoodPage() {
   return (
     <Suspense fallback={<div>Loading menu...</div>}>
