47d77fec258cd76eedd01991b3b242d8b3df7bad
Hari Singh Joshi <jiffymenu@gmail.com>
2025-11-10 11:18:22 -0500

    Remove sticky positioning from cart aside

--- app/food/FoodClient.tsx
+++ app/food/FoodClient.tsx
@@ -254,7 +254,8 @@ export default function FoodMenuPage() {
         </section>
 
         {/* Cart */}
-       <aside className="space-y-4 sticky top-4 self-start h-fit">
+     <aside className="space-y-4">
+
 
           <h2 className="text-lg font-semibold flex items-center">
             <ShoppingCart className="h-5 w-5 mr-1" /> Cart
